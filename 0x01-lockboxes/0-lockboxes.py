edb601356e25150dbab277a1ffb42d15ec2c47d1
Osaclay <osaosun@gmail.com>
2023-07-05 06:38:51 +0000

    lock box puzzle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,64 @@
 #!/usr/bin/python3
 """Solves the lock boxes puzzle """
+
+
+def look_next_opened_box(opened_boxes):
+    """Looks for the next opened box
+    Args:
+        opened_boxes (dict): Dictionary which contains boxes already opened
+    Returns:
+        list: List with the keys contained in the opened box
+    """
+    for index, box in opened_boxes.items():
+        if box.get('status') == 'opened':
+            box['status'] = 'opened/checked'
+            return box.get('keys')
+    return None
+
+
 def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n
-    stack = [0]  # Start with the first box (boxes[0])
+    """Check if all boxes can be opened
+    Args:
+        boxes (list): List which contains all the boxes with the keys
+    Returns:
+        bool: True if all boxes can be opened, otherwise, False
+    """
+    if len(boxes) <= 1 or boxes == [[]]:
+        return True
 
-    while stack:
-        box = stack.pop()
-        if not visited[box]:
-            visited[box] = True
-            for key in boxes[box]:
-                if key < n:
-                    stack.append(key)
+    opened_boxes = {}
+    while True:
+        if len(opened_boxes) == 0:
+            opened_boxes[0] = {
+                'status': 'opened',
+                'keys': boxes[0],
+            }
+        keys = look_next_opened_box(opened_boxes)
+        if keys:
+            for key in keys:
+                try:
+                    if opened_boxes.get(key) and opened_boxes.get(key).get('status') == 'opened/checked':
+                        continue
+                    opened_boxes[key] = {
+                        'status': 'opened',
+                        'keys': boxes[key]
+                    }
+                except (KeyError, IndexError):
+                    continue
+        elif 'opened' in [box.get('status') for box in opened_boxes.values()]:
+            continue
+        elif len(opened_boxes) == len(boxes):
+            break
+        else:
+            return False
 
-    return all(visited)
+    return len(opened_boxes) == len(boxes)
+
+
+def main():
+    """Entry point"""
+    canUnlockAll([[]])
+
+
+if __name__ == '__main__':
+    main()
