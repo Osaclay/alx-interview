1096b0a814202b22f405a299602cf0aa21b4b375
Osaclay <osaosun>
2023-07-18 10:44:22 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,48 +2,54 @@
 
 import sys
 
-def print_stats(total_size, status_codes):
-    print("File size: {}".format(total_size))
-    sorted_status_codes = sorted(status_codes.keys())
-    for code in sorted_status_codes:
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
 
-def parse_line(line):
-    parts = line.split()
-    if len(parts) >= 7:
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-        return status_code, file_size
-    return None, None
+def print_msg(status_code_counts, total_file_size):
+    """
+    Method to print
+    Args:
+        status_code_counts: dictionary of status code counts
+        total_file_size: total file size
+    Returns:
+        Nothing
+    """
 
-if __name__ == "__main__":
-    total_size = 0
-    status_codes = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(status_code_counts.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-    try:
-        count = 0
-        for line in sys.stdin:
-            status_code, file_size = parse_line(line)
-            if status_code and file_size:
-                total_size += file_size
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
 
-            count += 1
-            if count == 10:
-                print_stats(total_size, status_codes)
-                count = 0
+total_file_size = 0
+status_code = 0
+line_counter = 0
+status_code_counts = {"200": 0,
+                     "301": 0,
+                     "400": 0,
+                     "401": 0,
+                     "403": 0,
+                     "404": 0,
+                     "405": 0,
+                     "500": 0}
 
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-        raise
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
+            line_counter += 1
+
+            if line_counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                status_code = parsed_line[1]  # status code
+
+                if status_code in status_code_counts.keys():
+                    status_code_counts[status_code] += 1
+
+            if line_counter == 10:
+                print_msg(status_code_counts, total_file_size)
+                line_counter = 0
+
+finally:
+    print_msg(status_code_counts, total_file_size)
+
