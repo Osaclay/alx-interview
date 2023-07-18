253a5e54fd5c1ee99f69c143e136872d998053dd
Osaclay <osaosun>
2023-07-18 10:47:28 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,33 +3,33 @@
 import sys
 
 
-def print_msg(status_code_counts, total_file_size):
+def print_msg(dict_sc, total_file_size):
     """
     Method to print
     Args:
-        status_code_counts: dictionary of status code counts
-        total_file_size: total file size
+        dict_sc: dict of status codes
+        total_file_size: total of the file
     Returns:
         Nothing
     """
 
     print("File size: {}".format(total_file_size))
-    for key, val in sorted(status_code_counts.items()):
+    for key, val in sorted(dict_sc.items()):
         if val != 0:
             print("{}: {}".format(key, val))
 
 
 total_file_size = 0
-status_code = 0
-line_counter = 0
-status_code_counts = {"200": 0,
-                     "301": 0,
-                     "400": 0,
-                     "401": 0,
-                     "403": 0,
-                     "404": 0,
-                     "405": 0,
-                     "500": 0}
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 try:
     for line in sys.stdin:
@@ -37,19 +37,18 @@ try:
         parsed_line = parsed_line[::-1]  # inverting
 
         if len(parsed_line) > 2:
-            line_counter += 1
+            counter += 1
 
-            if line_counter <= 10:
+            if counter <= 10:
                 total_file_size += int(parsed_line[0])  # file size
-                status_code = parsed_line[1]  # status code
+                code = parsed_line[1]  # status code
 
-                if status_code in status_code_counts.keys():
-                    status_code_counts[status_code] += 1
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
 
-            if line_counter == 10:
-                print_msg(status_code_counts, total_file_size)
-                line_counter = 0
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
 
 finally:
-    print_msg(status_code_counts, total_file_size)
-
+    print_msg(dict_sc, total_file_size)
