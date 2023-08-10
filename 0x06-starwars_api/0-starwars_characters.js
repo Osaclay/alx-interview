b968bf560eacdf5c39b9343a5fb146289e135d67
Osaclay <osaosun@gmail.com>
2023-08-09 21:29:29 -0700

    starwars characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,46 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
+const httpRequest = require('request');
 
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Invalid response:', response.statusCode);
-    process.exit(1);
-  }
-
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  const fetchCharacter = (characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        process.exit(1);
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Invalid response:', charResponse.statusCode);
-        process.exit(1);
-      }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-    });
-  };
-
-  for (const characterUrl of characters) {
-    fetchCharacter(characterUrl);
-  }
+httpRequest('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, responseBody) {
+  if (error) throw error;
+  const characterUrls = JSON.parse(responseBody).characters;
+  processCharacters(characterUrls, 0);
 });
+
+const processCharacters = (characterUrls, index) => {
+  if (index === characterUrls.length) return;
+  httpRequest(characterUrls[index], function (error, response, responseBody) {
+    if (error) throw error;
+    console.log(JSON.parse(responseBody).name);
+    processCharacters(characterUrls, index + 1);
+  });
+};
