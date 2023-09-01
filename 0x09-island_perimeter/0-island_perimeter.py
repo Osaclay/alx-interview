68777e3d696212ed4911db4cd5e61ff16a015050
Osaclay <osaosun@gmail.com>
2023-09-01 04:46:19 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,16 +14,16 @@ def island_perimeter(grid):
         int: The perimeter of the island.
     """
 
-    perimeter = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if (grid[row][col] == 1):
-                if (row <= 0 or grid[row - 1][col] == 0):
-                    perimeter += 1
-                if (row >= len(grid) - 1 or grid[row + 1][col] == 0):
-                    perimeter += 1
-                if (col <= 0 or grid[row][col - 1] == 0):
-                    perimeter += 1
-                if (col >= len(grid[row]) - 1 or grid[row][col + 1] == 0):
-                    perimeter += 1
-    return perimeter
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    p += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    p += 1
+    return p
