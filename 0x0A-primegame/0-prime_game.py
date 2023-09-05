8c926d44fbc57b7265e6af3821584d7f87cf16ac
Osaclay <osaosun@gmail.com>
2023-09-05 02:26:39 +0000

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def isWinner(x, nums):
+    if x <= 0 or not nums:
+        return None
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif maria_wins < ben_wins:
+        return "Ben"
+    else:
+        return None
